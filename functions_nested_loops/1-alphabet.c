7278abb5133bb68827ad60e6628b0ea86ccc5f32
NicoV00 <6328@holbertonstudents.com>
2023-02-17 07:30:22 -0800

    nested loops

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *  * main - Entry point 
+ *   * Return: Always 0 (Success)
+ *    **/
+int main(void)
+{
+printf("abcdefghijklmnopqrstuvwxyz");
+putchar('\n');
+return (0);
+}
