030fdc3a0e622c99c902291c30657b152d8d4687
NicoV00 <6328@holbertonstudents.com>
2023-02-17 09:53:28 -0800

    nested loops

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -15,16 +15,16 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar('+');
+		putchar(43);
 		return (1);
 	}
-	else if (n == 0)
+	else if (n < 0)
 	{
-		putchar('0');
+		putchar(45);
 		return (0);
 	}
 	else
 	{
-		putchar('-');
+		putchar(48;
 		return (1);	}
 }
