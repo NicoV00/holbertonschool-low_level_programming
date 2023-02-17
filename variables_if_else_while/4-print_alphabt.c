17f036dd91e54fb3c2a1f7d419087feffd427b86
NicoV00 <6328@holbertonstudents.com>
2023-02-17 04:48:35 -0800

    variables

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,16 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
+
 /**
- * main - program that prints the alphabet in lowercase and the uppercase
- * You can only use two putchar
- * Return: 0
- */
+ *  * main - Entry point
+ *   * Return: Always 0 (Success)
+ *    **/
 int main(void)
 {
-char letter = 'a';
-while (letter <= 'z')
-if (letter != 'e' && letter != 'q')
-putchar(letter);
-}
-putchar('\n');
-return (0);
+	char *abc = "abcdfghijklmnoprstuvwxyz";
+	int letra;
+
+	for (letra = 0; letra <= 23; letra++)
+		putchar(abc[letra]);
+	putchar('\n');
+	return (0);
 }
