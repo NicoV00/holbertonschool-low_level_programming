7211ba55078352de49d952397d1b9c9f4bb8d0b4
NicoV00 <6328@holbertonstudents.com>
2023-02-17 07:21:53 -0800

    nested loops

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
-  * main - Entry point
-  *
-  * Return: Always 0
-  */
+ * main - program that prints _putchar
+ *
+ * Return: int
+ */
+
 int main(void)
 {
-_putchar('_putchar');
-_putchar('\n');
+	int n = 0;
+	char str_hbt[8] = "_putchar";
 
+	while (n < 8)
+	{
+		_putchar(str_hbt[n]);
+		n += 1;
+	}
+	_putchar('\n');
 	return (0);
 }
