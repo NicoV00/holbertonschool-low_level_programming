121febd3d63e3ef3b0b4ef03819a3bd234c13a4f
NicoV00 <6328@holbertonstudents.com>
2023-02-17 06:14:03 -0800

    variables

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print all single-digit numbers
+ * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+    int x;
+
+    for (x = 48; x < 58; x++)
+    {
+        putchar(x);
+        if (x < 57)
+        {
+            putchar(44);
+            putchar(32);
+        }
+    }
+    putchar('\n');
+    return (0);
+}
