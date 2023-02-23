c601aae44c068e60086e6f38fe65bb074062a8ac
NicoV00 <6328@holbertonstudents.com>
2023-02-23 05:44:27 -0800

    more nested loops

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - checks for uppercase character.
+ * @a: int
+ * Return: 1: c is uppercase. 0: is lowercase.
+ */
+int _isupper(int a)
+{
+	if (isupper((a)
+		return (1);
+    return (0);
+}
