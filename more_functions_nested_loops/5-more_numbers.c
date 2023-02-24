a678c33f2a731e62c04809896175796753b0c5f1
NicoV00 <6328@holbertonstudents.com>
2023-02-24 05:47:26 -0800

    more nested loops

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers - prints 0 to 14 
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+	char *num = "01234567891011121314";
+	int letra;
+	int cont;
+
+	for (cont = 0; cont <= 9; cont++)
+	{
+		for (letra = 0; letra <= 19; letra++)
+		{
+			putchar(num[letra]);
+		}
+		putchar('\n');
+	}
+}
