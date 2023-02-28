67365c03b9cdee47f8e8e9a3ef1bce5275750d06
NicoV00 <6328@holbertonstudents.com>
2023-02-28 08:45:17 -0800

    pointerss

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse
+ * 
+ * @s: String printed
+ *
+ * Return: nothing.
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+		i += 1;
+	i -= 1;
+	while (i >= 0)
+	{
+		_putchar(*(s + i));
+		i--;
+	}
+	_putchar('\n');
+}
