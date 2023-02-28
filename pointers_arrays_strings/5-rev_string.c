5932ecb16c15735d0e339df3e31ec65501ca9ad1
NicoV00 <6328@holbertonstudents.com>
2023-02-28 10:07:06 -0800

    pointerss

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: string
+ *Return: Always 0.
+ **/
+void rev_string(char *s)
+{
+	int x, y;
+	char intercb;
+
+	for (x = 0; s[x] != '\0'; x++)
+		;
+	for (y = 0; y < x / 2; y++)
+
+	{
+		intercb = s[y];
+		s[y] = s[x - 1 - y];
+		s[x - 1 - y] = intercb;
+	}
+}
