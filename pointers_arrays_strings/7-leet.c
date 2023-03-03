1af552f8859dbf1c0a34291298c7c7713d6331b8
NicoV00 <6328@holbertonstudents.com>
2023-03-03 11:08:41 -0800

    pointerss

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* leet - encodes a string into 1337.
+* @s: puntero char
+* Return: 0
+*/
+char *leet(char *s)
+{
+	int i = 0;
+	int j = 0;
+	char *l = "aAeEoOlLtT";
+	char *n = "4433001177";
+
+	while (*(s + i) != '\0')
+	{
+		while (*(l + j) != '\0')
+		{
+			if (*(s + i) == *(l + j))
+				*(s + i)  = *(n + j);
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	return (s);
+}
