7c0602197f58d10fb8322c08da9302164eabae23
NicoV00 <6328@holbertonstudents.com>
2023-03-06 05:30:51 -0800

    pointerss

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * _memcpy - copies memory area
@@ -12,11 +12,10 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < n; i+)
+	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
 	}
 
 	return (dest);
 }
-
