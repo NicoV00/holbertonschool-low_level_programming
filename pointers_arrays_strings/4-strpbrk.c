420e6705976fd10eefc658e4f4f526b724406a11
NicoV00 <6328@holbertonstudents.com>
2023-03-06 06:06:15 -0800

    pointerss

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strpbrk - function that searches a string for any of a set of bytes 
+ * _strpbrk - function that searches a string for any of a set of bytes
  *
  * @s: string
  * @accept: bytes in string
