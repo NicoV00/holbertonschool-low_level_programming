1a4e0d3e22b391fbecd16bab9d6b030d670dce00
NicoV00 <6328@holbertonstudents.com>
2023-03-06 09:11:47 -0800

    recursiv

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,1 +1,17 @@
 #include "main.h"
+
+/**
+ * _puts_recursion - prints a string in reverse
+ * @s: string
+ * Return: 0.
+ **/
+
+void _print_rev_recursion(char *s)
+{
+
+	if (*s)
+	{
+		_print_rev_recursion(s);
+		_putchar(*s);
+	}
+}
