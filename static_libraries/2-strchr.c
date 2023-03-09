dc5aa1a1cc689f8477c39a6b6022d462b34ac6b7
NicoV00 <6328@holbertonstudents.com>
2023-03-09 10:51:56 -0800

    static

--- static_libraries/2-strchr.c
+++ static_libraries/2-strchr.c
@@ -1,10 +1,11 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * _strchr - function that locates a character in a string.
  * @s: string
  * @c: character
- * Return: pointer a char
+ * Return: NULL.
  */
 
 char *_strchr(char *s, char c)
