e229717ef9e6bdf0f92b11ce87166d7bd5efeb79
NicoV00 <6328@holbertonstudents.com>
2023-03-12 12:27:31 -0700

    args.c hopefully running

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints numer of arguments
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
