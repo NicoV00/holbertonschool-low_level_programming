f0824b6b79730674d0560bcac1ad9025a3be6180
NicoV00 <6328@holbertonstudents.com>
2023-03-12 10:10:45 -0700

    recursiv

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _funcionSQR - calcula SRQ
+ * @n: int
+ * @x: int
+ * Return: int
+ */
+
+int  _funcionSQR(int n, int x)
+{
+	if (x * x == n)
+		return (x);
+
+	if (x * x > n)
+		return (-1);
+
+	return (_funcionSQR(n, x + 1));
+}
+
+/**
+ * _sqrt_recursion -  returns natural square root of a number
+ *
+ * @n: int
+ *
+ * Return: int
+ */
+
+int  _sqrt_recursion(int n)
+{
+	return (funcionSQR(n, 0));
+}
