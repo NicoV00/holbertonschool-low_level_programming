c26988ea5613cd35fb68f559140fa54a3e78a66a
NicoV00 <6328@holbertonstudents.com>
2023-03-12 13:28:25 -0700

    recursiv

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/*
+ * check - checks if a number is divisible
+ * @n: int
+ * @x: int
+ * Return: int
+ */
+int check(int n, int x)
+{
+	if (n == x)
+		return (1);
+	if (n % x == 0)
+		return (0);
+	return (check(n, x + 1));
+}
+
+/*
+ * is_prime_number - check if number is prime
+ * @n: int
+ * Return: int
+ */
+
+int is_prime_number(int n)
+{
+	if (n <=)
+		return (0);
+	return (check(n, 2));
+}
