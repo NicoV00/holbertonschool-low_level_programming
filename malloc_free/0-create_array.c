c024309b52cf41643432f235813de358c9c65c81
NicoV00 <6328@holbertonstudents.com>
2023-03-24 08:50:57 -0700

    Late mallloc

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars
+ * @size: array sie
+ * @c: char
+ * Return: 0
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *a;
+
+	a =  malloc(size * sizeof(char));
+	if (a == NULL)
+		return (NULL);
+
+	if (size == 0)
+		return ('\0');
+	
+	for (i = 0; i <= size; i++)
+		a[i] = c;
+	return (a);
+}
