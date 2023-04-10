b82e22df8f89bc63da39ebf494c4b0dc6cd081f8
NicoV00 <6328@holbertonstudents.com>
2023-04-10 10:19:14 -0700

    fixed spaces

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -4,29 +4,28 @@
 #include <string.h>
 
 /* add_node_end - adds node at end
- * 
  * @head: head
  * @str: string to add
  * Return: address of the new element
- */
+**/
 
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new = malloc(sizeof(list_t));
 	list_t *temp = *head;
-	
+
 	if (new == NULL)
 		return (NULL);
-	
+
 	new->str = strdup(str);
 	new->len = strlen(str);
-	
+
 	if (*head == NULL)
 	{
 		*head = new;
 		return (*head);
 	}
-	
+
 	while (temp->next != NULL)
 	{
 		temp = temp->next;
