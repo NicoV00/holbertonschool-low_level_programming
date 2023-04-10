1b3938260332ae917ef8f0945b7febbf7dec0296
NicoV00 <6328@holbertonstudents.com>
2023-04-10 11:13:55 -0700

    fixed kernel

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -11,18 +11,19 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 	new = malloc(sizeof(dlistint_t));
+	
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
 	new->prev = NULL;
-	
+
 	if (*head == NULL)
 	{
 		new->next = NULL;
 		*head = new;
 		return (*head);
 	}
-	
+
 	new->next = *head;
 	(*head)->prev = new;
 	*head = new;
