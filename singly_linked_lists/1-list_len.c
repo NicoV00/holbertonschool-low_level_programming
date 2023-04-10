b38f39780a8ec2b08d39b8c9f290109147330ac9
NicoV00 <6328@holbertonstudents.com>
2023-04-10 09:03:54 -0700

    keep going

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list_t list
+ * @h: head
+ * Return: number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t x;
+	
+	x = 0;
+	
+	while (h != NULL)
+	{
+		x++;
+		h = h->next;
+	}
+	return (x);
+}
