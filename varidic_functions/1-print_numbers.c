271580c74a5e6e9f48ade51bfd5532ca82599651
NicoV00 <6328@holbertonstudents.com>
2023-04-30 20:06:18 -0700

    varidic functions uploaded from visual

---
+++ varidic_functions/1-print_numbers.c
@@ -0,0 +1,34 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ *  * print_numbers - prints numbers
+ *   * @separator: separator char
+ *    * @n: n of ints passed
+ *     * Return: void
+ *      */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+		unsigned int i = 0;
+			va_list list;
+
+				va_start(list, n);
+					if (separator == NULL)
+								separator = "";
+
+						for (i = 0; i < n; i++)
+								{
+											if (i == n - 1)
+														{
+																		printf("%d", va_arg(list, int));
+																					continue;
+																							}
+													printf("%d%s", va_arg(list, int), separator);
+														}
+							printf("\n");
+								va_end(list);
+
+
+
+}
