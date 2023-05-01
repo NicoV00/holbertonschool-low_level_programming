0e59d35df6db1b0d12be0fd9721d41827aa3be43
NicoV00 <6328@holbertonstudents.com>
2023-04-30 19:12:49 -0700

    malloc

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/*
+ * malloc_checked -  allocate memory using malloc
+ * @b: int
+ * Return: a pointer to the memory allocated
+ */
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *array;
+	
+	array = malloc(b);
+	
+	if (array == NULL)
+	{
+		free(array);
+		exit(98);
+	}
+	return (array);
+}
